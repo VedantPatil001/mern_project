cfc0d5b89017101a3f726718e11afb694371008a
vedant Patil <vedantspatil28082005@gmail.com>
2024-08-06 18:48:37 +0530

    first commit

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
+function Homepage() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={1}
+        bg="white"
+        w="100%"
+        m="12px 0 5px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="3xl" fontFamily="Work sans">
+          Chat with Fridens
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={1} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+}
+
+export default Homepage;
