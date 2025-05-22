38a9416e718eebad3c4c8201ec0bd1a1d77fddca
vedant Patil <vedantspatil28082005@gmail.com>
2025-05-23 00:10:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,17 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // to accept json data
+
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-
+// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -33,7 +37,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// --------------------------deployment------------------------------
 
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
@@ -48,7 +54,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:3000",
-    
+    // credentials: true,
   },
 });
 
