7acfb941d94097acc4636d1df5bd6015889d44e2
vedant Patil <vedantspatil28082005@gmail.com>
2025-05-27 16:02:05 +0530

    Update SingleChat.js

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -16,7 +16,7 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
 const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
-// var socket, selectedChatCompare;
+var selectedChatCompare;
 
 const socket = io("http://localhost:5000", {
   withCredentials: true,
