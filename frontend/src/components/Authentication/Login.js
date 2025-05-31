ff67ee32edca0f79a92b3997c0794145738c21b1
vedant Patil <vedantspatil28082005@gmail.com>
2025-05-31 17:43:45 +0530

    Update Login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -106,17 +106,7 @@ const Login = () => {
       >
         Login
       </Button>
-      <Button
-        variant="solid"
-        colorScheme="red"
-        width="100%"
-        onClick={() => {
-          setEmail("guest@example.com");
-          setPassword("123456");
-        }}
-      >
-        Get Guest User Credentials
-      </Button>
+      
     </VStack>
   );
 };
